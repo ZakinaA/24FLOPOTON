9f8246d8ff2dd4a321c4a08ae19374d03994d020
Minh-Tri <nguyen.minhtri@outlook.fr>
2024-11-13 11:39:45 +0100

    changement des différentes valeurs

--- src/Controller/TypeInstrumentController.php
+++ src/Controller/TypeInstrumentController.php
@@ -8,24 +8,24 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\TypeInstrument;
 use Doctrine\Persistence\ManagerRegistry;
 
-#[Route('/typeinstrument', name: 'typeinstrument_')]
+#[Route('/typeinstrument', name: 'app_typeinstrument_')]
 class TypeInstrumentController extends AbstractController
 {
     #[Route('/index', name: 'index')]
     function accueil(): Response
     {
         $annee = '2024';
-        return $this->render('etudiant/accueil.html.twig', ['pAnnee' => $annee,
+        return $this->render('type_instrument/accueil.html.twig', ['pAnnee' => $annee,
        ]);	
     }
 
     #[Route('/lister', name: 'lister')]
-    function etudiantLister(ManagerRegistry $doctrine){
+    function typeinstrumentLister(ManagerRegistry $doctrine){
 
-        $repository = $doctrine->getRepository(Etudiant::class);
+        $repository = $doctrine->getRepository(TypeInstrument::class);
 
     $typesinstruments= $repository->findAll();
-    return $this->render('typeinstrument/lister.html.twig', [
+    return $this->render('type_instrument/lister.html.twig', [
         'tTypesInstruments' => $typesinstruments,]);
     }
 }
