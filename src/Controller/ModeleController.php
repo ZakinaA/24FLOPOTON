7be2eb96772e5a4c815007a0633a6fdeee675c36
Minh-Tri <nguyen.minhtri@outlook.fr>
2024-11-18 10:45:36 +0100

    Ajout de la fonction permettant de lister les modèles

--- src/Controller/ModeleController.php
+++ src/Controller/ModeleController.php
@@ -5,14 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Modele;
+use Doctrine\Persistence\ManagerRegistry;
 
+#[Route('/modele', name: 'app_modele_')]
 class ModeleController extends AbstractController
 {
-    #[Route('/modele', name: 'app_modele')]
-    public function index(): Response
+    #[Route('/index', name: 'index')]
+    function accueil(): Response
     {
-        return $this->render('modele/index.html.twig', [
-            'controller_name' => 'ModeleController',
-        ]);
+        $annee = '2024';
+        return $this->render('modele/index.html.twig', ['pAnnee' => $annee,
+       ]);	
+    }
+
+    #[Route('/lister', name: 'lister')]
+    function modeleLister(ManagerRegistry $doctrine){
+
+        $repository = $doctrine->getRepository(Modele::class);
+
+    $modeles= $repository->findAll();
+    return $this->render('modele/lister.html.twig', [
+        'mModele' => $modeles,]);
     }
 }
