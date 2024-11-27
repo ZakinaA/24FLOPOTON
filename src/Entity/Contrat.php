83eee356dbd3ad8c43dba8eb9c6a59171a7829bb
MatysA <matysallanet@gmail.com>
2024-11-27 11:54:33 +0100

    création de listerContrat et création de l'entity contrat avec ses relations

---
+++ src/Entity/Contrat.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContratRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContratRepository::class)]
+class Contrat
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateDebut = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateFin = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $etatDetailleDebut = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $etatDetailleFin = null;
+
+    #[ORM\ManyToOne(inversedBy: 'contrats')]
+    private ?Eleve $eleve = null;
+
+    #[ORM\ManyToOne(inversedBy: 'contrats')]
+    private ?Instrument $instrument = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $dateDebut): static
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(\DateTimeInterface $dateFin): static
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    public function getEtatDetailleDebut(): ?string
+    {
+        return $this->etatDetailleDebut;
+    }
+
+    public function setEtatDetailleDebut(string $etatDetailleDebut): static
+    {
+        $this->etatDetailleDebut = $etatDetailleDebut;
+
+        return $this;
+    }
+
+    public function getEtatDetailleFin(): ?string
+    {
+        return $this->etatDetailleFin;
+    }
+
+    public function setEtatDetailleFin(string $etatDetailleFin): static
+    {
+        $this->etatDetailleFin = $etatDetailleFin;
+
+        return $this;
+    }
+
+    public function getEleve(): ?Eleve
+    {
+        return $this->eleve;
+    }
+
+    public function setEleve(?Eleve $eleve): static
+    {
+        $this->eleve = $eleve;
+
+        return $this;
+    }
+
+    public function getInstrument(): ?Instrument
+    {
+        return $this->instrument;
+    }
+
+    public function setInstrument(?Instrument $instrument): static
+    {
+        $this->instrument = $instrument;
+
+        return $this;
+    }
+}
