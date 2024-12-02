f4f45f58eaf4c9b6501a1d72854c3cf138f78d94
Baptiste <baptiste.lerossignol@gmail.com>
2024-12-02 16:56:12 +0100

    vue & controller ajouter quotient

---
+++ src/Form/QuotientFamilialEditType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\QuotientFamilial;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuotientFamilialEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle', TextType::class, ['label' => 'Libelle'])
+            ->add('quotientMini', NumberType::class, ['label' => 'Quotient minimum'])
+            ->add('save', SubmitType::class, ['label' => 'Modifier'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => QuotientFamilial::class,
+        ]);
+    }
+}
