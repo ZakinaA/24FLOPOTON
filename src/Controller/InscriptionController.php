47bd153cddb5d0f77729e8a37b84d516de6bf6c9
bocquet <matheo.bocquet18@gmail.com>
2024-12-02 15:45:11 +0100

    ajouter inscription

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Inscription;
 use Doctrine\Persistence\ManagerRegistry;
-use App\Form\CoursType;
+use App\Form\InscriptionType;
 use App\Form\CoursModifierType;
 
 #[Route(path: '/inscription', name: 'app_inscription_')]
@@ -32,5 +32,24 @@ class InscriptionController extends AbstractController
         $inscription= $repository->findAll();
         return $this->render('inscription/lister.html.twig', [
             'pInscription' => $inscription,]);
+    }
+
+    #[Route('/ajouter', name: 'ajouter')]
+    public function ajouter(ManagerRegistry $doctrine, Request $request){
+        $inscription = new Inscription();
+        $form = $this->createForm(InscriptionType::class, $inscription);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $inscription = $form->getData();
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($inscription);
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('app_inscription_lister');
+        } else  {
+            return $this->render('inscription/ajouter.html.twig', array('form' => $form->createView(),));
+        }
     }
 }
