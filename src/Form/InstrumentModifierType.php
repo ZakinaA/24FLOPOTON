32f37bae29fc2a3a9dfdd76615b4de997b4544b5
Minh-Tri <nguyen.minhtri@outlook.fr>
2024-12-04 10:52:40 +0100

    ajout d'un bouton lors de la modification d'une donnée d'un instrument

--- src/Form/InstrumentModifierType.php
+++ src/Form/InstrumentModifierType.php
@@ -10,6 +10,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class InstrumentModifierType extends AbstractType
 {
@@ -35,6 +37,7 @@ class InstrumentModifierType extends AbstractType
                 'class' => Modele::class,
                 'choice_label' => 'id',
             ])
+            ->add('enregistrer', SubmitType::class, array('label' => 'Modifier instrument'))
         ;
     }
 
