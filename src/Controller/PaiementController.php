865e451b4934fb16ae0f64dc409616db37f54206
bocquet <matheo.bocquet18@gmail.com>
2024-12-09 11:09:54 +0100

    fonction ajouter modifier et supprimer paiement

--- src/Controller/PaiementController.php
+++ src/Controller/PaiementController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Paiement;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Form\PaiementType;
+use App\Form\PaiementModifierType;
 
 #[Route('/paiement', name: 'app_paiement_')]
 class PaiementController extends AbstractController
@@ -30,4 +32,68 @@ class PaiementController extends AbstractController
             'pPaiement' => $paiement,]);
     }
 
+    #[Route('/ajouter', name: 'ajouter')]
+    public function ajouter(ManagerRegistry $doctrine, Request $request){
+        $paiement = new Paiement();
+        $form = $this->createForm(PaiementType::class, $paiement);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $paiement = $form->getData();
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($paiement);
+            $entityManager->flush();
+            
+            return $this->render('paiement/consulter.html.twig', ['paiement' => $paiement,]);
+        } else  {
+            return $this->render('paiement/ajouter.html.twig', array('form' => $form->createView(),));
+        }
+    }
+
+    #[Route('/modifier/{id}', name: 'modifier')]
+
+    public function modifierPaiement(ManagerRegistry $doctrine, $id, Request $request){
+ 
+        //récupération du cours dont l'id est passé en paramètre
+        $paiement = $doctrine->getRepository(Paiement::class)->find($id);
+     
+        if (!$paiement) {
+            throw $this->createNotFoundException('Aucun paiement trouvé avec le numéro '.$id);
+        }
+        else
+        {
+                $form = $this->createForm(PaiementModifierType::class, $paiement);
+                $form->handleRequest($request);
+     
+                if ($form->isSubmitted() && $form->isValid()) {
+     
+                     $paiement = $form->getData();
+                     $entityManager = $doctrine->getManager();
+                     $entityManager->persist($paiement);
+                     $entityManager->flush();
+                     return $this->redirectToRoute('app_paiement_lister');
+               }
+               else{
+                    return $this->render('paiement/ajouter.html.twig', array('form' => $form->createView(),));
+               }
+            }
+     }
+
+     #[Route('/supprimer/{id}', name: 'supprimer')]
+
+     public function supprimer(ManagerRegistry $doctrine, int $id): Response
+     {
+         $paiement = $doctrine->getRepository(Paiement::class)->find($id);
+ 
+         if (!$paiement) {
+             throw $this->createNotFoundException('Aucun paiement trouvé avec l\'ID '.$id);
+         }
+ 
+         $entityManager = $doctrine->getManager();
+         $entityManager->remove($paiement); 
+         $entityManager->flush();
+ 
+         return $this->redirectToRoute('app_paiement_lister');
+     }
 }
