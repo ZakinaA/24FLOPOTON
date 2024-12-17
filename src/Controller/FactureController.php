fcaff700a63737f8a3b01e3e871bd9f8aa304e33
BaptLR <baba1408.mail@gmail.com>
2024-12-17 23:49:42 +0100

    Correction Facture

--- src/Controller/FactureController.php
+++ src/Controller/FactureController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\Inscription;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,20 +22,30 @@ class FactureController extends AbstractController
     #[Route('/lister', name: 'lister')]
     public function lister(ManagerRegistry $doctrine, Security $security){
         $repository = $doctrine->getRepository(Inscription::class);
-        $entities = $repository->findBy();
+        $entities = $repository->findAll();
 
-        $headers = ['Élève', 'Cours', 'Heure début', 'Jour', 'Prix'];
+        $headers = ['Élève', 'Cours', 'Type de Cours', 'Heure début', 'Jour', 'Prix'];
         $rows = [];
 
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if(!$user->getResponsable()){
+            throw $this->createNotFoundException('Vous \'êtes pas un responsable d\'élève');
+        }
+
         foreach ($entities as $e) {
-            $rows[] = [
-                $e->getId(),
-                $e->getEleve()?->getPrenom(), 
-                $e->getCours()?->getLibelle(),
-                $e->getCours()?->getHeureDebut()?->format('H:i'),
-                $e->getCours()?->getJour()?->getLibelle(),
-                $e->getCours()?->getTypecours()?->getTarif()
-            ];
+            if($e->getEleve()->getResponsable()->getId() == $user->getResponsable()->getId()){
+                $rows[] = [
+                    $e->getId(),
+                    $e->getEleve()?->getPrenom(), 
+                    $e->getCours()?->getLibelle(),
+                    $e->getCours()?->getTypecours()?->getLibelle(),
+                    $e->getCours()?->getHeureDebut()?->format('H:i'),
+                    $e->getCours()?->getJour()?->getLibelle(),
+                    $e->getCours()?->getTypecours()?->getTarif()?->getMontant()
+                ];
+            }
         }
 
         return $this->render('entities/lister.html.twig', [
